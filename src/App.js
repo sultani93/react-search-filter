e633e4b375afd23950e16ba2152631b534c22618
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2022-12-05 13:58:50 +0400

    function done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Table from './Table'
+import { Users } from './users'
 
 function App() {
+  const [query, setQuery] = useState('')
+  const keys = ['first_name', 'last_name', 'email']
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <input
+        type='text'
+        className='search'
+        placeholder='Search...'
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={search(Users)} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
